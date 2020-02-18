d6b9f8a42632c7e46d05941e8037b857d3371226
Zekun-Yang <zekun.yang@outlook.com>
2020-02-18 20:30:13 +0000

    add routes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-import os 
+import os
 from flask import Flask
 from flask import jsonify
 from api.utils.database import db
 from api.config.config import ProductionConfig, TestingConfig, DevelopmentConfig
+from api.utils.responses import response_with
+import api.utils.responses as resp
+
 
 app = Flask(__name__)
 
@@ -13,13 +16,35 @@ elif os.environ.get('WORK_ENV') == 'TEST':
 else:
     app_config = DevelopmentConfig
 
-def create_app(config):
-    app = Flask(__name__)
-    app.config.from_object(app_config)
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-    return app
+
+app.config.from_object(app_config)
+
+
+
+@app.after_request
+def add_header(response):
+    return response
+
+@app.errorhandler(400)
+def bad_request(e):
+    logging.error(e)
+    return response_with(resp.BAD_REQUEST_400)
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.error(e)
+    return response_with(resp.SERVER_ERROR_500)
+
+@app.errorhandler(404)
+def not_found(e):
+    logging.error(e)
+    return response_with(resp. SERVER_ERROR_404)
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    
+    
 
 
 if __name__ == "__main__":
