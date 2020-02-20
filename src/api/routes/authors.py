123ead39160ae474f1d0553b23bfa3145051c3bb
Zekun-Yang <zekun.yang@outlook.com>
2020-02-20 12:12:43 +0000

    add author put delete

--- src/api/routes/authors.py
+++ src/api/routes/authors.py
@@ -36,3 +36,24 @@ def get_author_detail(author_id):
     author_schema = AuthorSchema()
     author = author_schema.dump(fetched)
     return response_with(resp.SUCCESS_200, value={"author":author})
+
+@author_routes.route('/<int:author_id>', methods=['PUT'])
+def update_author_detail(author_id):
+    data = request.get_json()
+    get_author = Author.query.get_or_404(author_id)
+    get_author.first_name = data['first_name']
+    get_author.last_name = data['last_name']
+    db.session.add(get_author)
+    db.session.commit()
+
+    author_schema = AuthorSchema()
+    author = author_schema.dump(get_author)
+    return response_with(resp.SUCCESS_200, value={"author":author})
+
+@author_routes.route('/<int:author_id>', methods=['DELETE'])
+def delete_author(author_id):
+    get_author = Author.query.get_or_404(author_id)
+    db.session.delete(get_author)
+    db.session.commit()
+    return response_with(resp.SUCCESS_204)
+    
