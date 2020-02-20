a7b36a6c82bfaab7064ac6182f4ad64bf5b3776d
Zekun-Yang <zekun.yang@outlook.com>
2020-02-20 16:57:28 +0000

    add book put and delete

--- src/api/routes/book.py
+++ src/api/routes/book.py
@@ -19,3 +19,41 @@ def create_book():
         print(e)
         return response_with(resp.INVALID_INPUT_422)
 
+@book_routes.route('/', methods=['GET'])
+def get_book_list():
+    fetched = Book.query.all()
+    book_schema = BookSchema(many=True, only=['author_id', 'title', 'year'])
+    books = book_schema.dump(fetched)
+    return response_with(resp.SUCCESS_200, value={"books":books})
+
+
+@book_routes.route('/<int:id>', methods=['GET'])
+def get_book_detail(id):
+    fetched = Book.query.get_or_404(id)
+    book_schema = BookSchema()
+    books = book_schema.dump(fetched)
+    return response_with(resp.SUCCESS_200, value={"book":books})
+
+@book_routes.route('/<int:id>', methods=['PUT'])
+def update_book_detail(id):
+    data = request.get_json()
+    get_book = Book.query.get_or_404(id)
+    get_book.title = data['title']
+    get_book.year = data['year']
+    db.session.add(get_book)
+    db.session.commit()
+    book_schema = BookSchema()
+    book = book_schema.dump(get_book)
+    return response_with(resp.SUCCESS_200, value={"book":book})
+
+@book_routes.route('/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    get_book = Book.query.get_or_404(id)
+    db.session.delete(get_book)
+    db.session.commit()
+    return response_with(resp.SUCCESS_204)
+
+
+
+
+
